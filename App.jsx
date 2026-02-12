35b4d29b14216aca537b557b133463a71a38445b
SARTHAK VERMA <118848237+sarthak162020@users.noreply.github.com>
2026-02-12 20:04:34 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1>Paradise Nursery</h1>
+        <p>
+          Welcome to Paradise Nursery – your one-stop destination for beautiful
+          indoor and outdoor plants that bring life to your home.
+        </p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
